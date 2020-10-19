2b60c968520eae9b842f019f8bed7e35e1c050fb
lior <mitrlior@gmail.com>
2020-10-19 15:25:40 +0300

    delte unsed methods

--- src/containers/tasksContainer.tsx
+++ src/containers/tasksContainer.tsx
@@ -2,12 +2,8 @@ import { observer } from "mobx-react";
 import React from "react";
 import AddTaskComponent from "../components/addTaskComponent";
 import FinishedTasksListComponent from "../components/finishedTasksListComponent";
-import TaskComponent from "../components/taskComponent";
 import TodoTasksListComponent from "../components/todoTasksListComponent";
-import Task from "../models/task.models";
-import rootStores from "../stores";
-import { TASKS_STORE } from "../stores/storesKeys";
-import "./TaskContainer.css"
+import "./TaskContainer.css";
 
 interface IProps {}
 
@@ -15,53 +11,13 @@ interface IState {}
 
 @observer
 export default class TaskContainer extends React.Component<IProps, IState> {
-  tasksStore = rootStores[TASKS_STORE];
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      currentItem: {
-        text: "",
-        key: 0,
-      },
-    };
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-  render() {
-    return (
-      <div className="Tasks">
-        <AddTaskComponent />
-        <TodoTasksListComponent/>
-        <FinishedTasksListComponent/>
-      </div>
-    );
-  }
-  displayTodo() {
-    var todoTasks = this.tasksStore.reveresedTodoTasks;
-    return todoTasks.map((task: Task) => {
-      return (
-        <TaskComponent
-          task={task}
-          handleOnClick={() => this.handleOnClick(task)}
-        />
-      );
-    });
-  }
-  displayFinished() {
-    var todoTasksFiniehed = this.tasksStore.reveresedFinishedTasks;
-    return todoTasksFiniehed.map((task: Task) => {
-      return (
-        <TaskComponent
-          task={task}
-          handleOnClick={() => this.handleOnClick(task)}
-        />
-      );
-    });
-  }
-  private handleOnClick(task: Task) {
-    if (task.getIsDone) {
-      this.tasksStore.removeTask(task);
-    } else {
-      this.tasksStore.doneTask(task);
-    }
-  }
+	render() {
+		return (
+			<div className="Tasks">
+				<AddTaskComponent />
+				<TodoTasksListComponent />
+				<FinishedTasksListComponent />
+			</div>
+		);
+	}
 }

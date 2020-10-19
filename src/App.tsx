98914ff2b11f6db4bdd4cb098bfd462ca241e14d
lior <mitrlior@gmail.com>
2020-10-19 16:16:49 +0300

    started routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { observer, Provider } from "mobx-react";
 import React from "react";
 import TaskContainer from "./containers/tasksContainer";
 import rootStores from "./stores";
+import { Route } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 @observer
 class App extends React.Component {
@@ -13,12 +15,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <Provider {...rootStores}>
-          <div className="App">
-            <TaskContainer />
-          </div>
-      </Provider>
-    );
+			<Provider {...rootStores}>
+				<BrowserRouter>
+					<div className="App">
+            <TaskContainer/>
+					</div>
+					<Route path="/Tasks" component={TaskContainer} />
+				</BrowserRouter>
+			</Provider>
+		);
   }
 }
 export default App;

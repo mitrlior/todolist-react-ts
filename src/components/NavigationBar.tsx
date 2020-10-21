c4cbc19df118e6b5f717b07db45c706d7cdefdfb
lior <mitrlior@gmail.com>
2020-10-21 19:31:24 +0300

    new navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -7,26 +7,11 @@ interface IState {}
 export default class NavigationBar extends React.Component<IProps, IState> {
 	render() {
 		return (
-			<div>
-				<nav>
-					<ul className="nav-links">
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/tasks">Tasks</Link>
-						</li>
-						<li>
-							<Link to="/todos">Todos</Link>
-						</li>
-						<li>
-							<Link to="/finished">Finished</Link>
-						</li>
-						<li>
-							<Link to="/signup">SignUp</Link>
-						</li>
-					</ul>
-				</nav>
+			<div className="navbar">
+					<Link to="/">Home</Link>
+					<Link to="/tasks">Tasks</Link>
+					<Link to="/todos">Todos</Link>
+					<Link to="/finished">Finished</Link>
 			</div>
 		);
 	}
